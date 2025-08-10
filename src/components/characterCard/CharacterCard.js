c0bd6e8e72c2ad995d0f3ddb9babb725ba00467d
Matheus <mlsilveira@minha.fag.edu.br>
2025-08-09 22:45:47 -0300

    layout finalizado

--- src/components/characterCard/CharacterCard.js
+++ src/components/characterCard/CharacterCard.js
@@ -22,56 +22,88 @@ export default function CharacterCard() {
               </div>
             </div> {/* finaliza div charaterid*/}
             <div className={styles.flex}> {/*inicia a div que divide a tela em duas colunas*/}
-              <div>
-                <p>elemento</p>
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }} >
+                <div className={styles.card}>
+                  <div className={styles.inerCard}>
+                    <p>Resistencia</p>
+                    <p>FOR</p>
+                    <p>DES</p>
+                    <p>CONS</p>
+                    <p>CHAR</p>
+                    <p>INT</p>
+                    <p>SAB</p>
+                  </div>
+                </div>
+                <div className={styles.card}>
+                  <div className={styles.inerCard}>
+                    <p>Acrobacia</p>
+                    <p>Arcanismo</p>
+                    <p>Atletismo</p>
+                    <p>Atuação</p>
+                    <p>Enganação</p>
+                    <p>Furtividade</p>
+                    <p>História</p>
+                    <p>Intimidação</p>
+                    <p>Intuição</p>
+                    <p>Investigação</p>
+                    <p>Lidar com Animais</p>
+                    <p>Medicina</p>
+                    <p>Natureza</p>
+                    <p>Percepção</p>
+                    <p>Persuasão</p>
+                    <p>Prestidigitação</p>
+                    <p>Religião</p>
+                    <p>Sobrevivência</p>
+                  </div>
+                </div>
               </div>
-              <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-                  <div className={styles.card}>
-                    <div className={styles.gridThree}>
-                      <div>18<br /> FOR</div>
-                      <div>10<br />DES</div>
-                      <div>14<br />CONS</div>
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                <div className={styles.card}>
+                  <div className={styles.gridThree}>
+                    <div>18<br /> FOR</div>
+                    <div>10<br />DES</div>
+                    <div>14<br />CONS</div>
+                  </div>
+                  <div className={styles.inerCard}>
+                    <div className={styles.barSection}>
+                      <div className={styles.barLabel}>Vida <span>30/30</span></div>
+                      <div className={`${styles.bar} ${styles.red}`}></div>
                     </div>
-                    <div className={styles.inerCard}>
-                      <div className={styles.barSection}>
-                        <div className={styles.barLabel}>Vida <span>30/30</span></div>
-                        <div className={`${styles.bar} ${styles.red}`}></div>
-                      </div>
+                  </div>
+                  <div className={styles.inerCard}>
+                    <div className={styles.barSection}>
+                      <div className={styles.barLabel}>Mana <span>30/30</span></div>
+                      <div className={`${styles.bar} ${styles.blue}`}></div>
                     </div>
-                    <div className={styles.inerCard}>
-                      <div className={styles.barSection}>
-                        <div className={styles.barLabel}>Mana <span>30/30</span></div>
-                        <div className={`${styles.bar} ${styles.blue}`}></div>
-                      </div>
-                    </div>
-                    <div className={styles.inerCard}>
-                      <div className={styles.skills}>
-                        <h3>Habilidades</h3>
-                        <div className={styles.dots}>
-                          {Array(8).fill(0).map((_, i) => (
-                            <div key={i} className={styles.dot}></div>
-                          ))}
-                        </div>
+                  </div>
+                  <div className={styles.inerCard}>
+                    <div className={styles.skills}>
+                      <h3>Habilidades</h3>
+                      <div className={styles.dots}>
+                        {Array(8).fill(0).map((_, i) => (
+                          <div key={i} className={styles.dot}></div>
+                        ))}
                       </div>
                     </div>
                   </div>
+                </div>
 
-                  <div className={styles.card}>
-                    <div className={styles.inerCard}>
-                      <h3>História</h3>
-                      <div>
-                        A Loba de Velyndor. No coração do continente de Eldaros, entre montanhas enevoadas e vales esquecidos...
-                      </div>
-                    </div>
-                    <div className={styles.inerCard} >
-                      <h3>Ligações</h3>
-                      <ul className={styles.list}>
-                        <li><span>Npc 1</span><span>100</span></li>
-                        <li><span>Npc 2</span><span>100</span></li>
-                        <li><span>Npc 3</span><span>100</span></li>
-                      </ul>
+                <div className={styles.card}>
+                  <div className={styles.inerCard}>
+                    <h3>História</h3>
+                    <div>
+                      A Loba de Velyndor. No coração do continente de Eldaros, entre montanhas enevoadas e vales esquecidos...
                     </div>
                   </div>
+                  <div className={styles.inerCard} >
+                    <h3>Ligações</h3>
+                    <ul className={styles.list}>
+                      <li><span>Npc 1</span><span>100</span></li>
+                      <li><span>Npc 2</span><span>100</span></li>
+                      <li><span>Npc 3</span><span>100</span></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
